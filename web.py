2fb93dc64fef4da426f8cd22062bf476dd830089
Oliver Gayagoy <olivergayagoy@Olivers-MacBook-Pro.local>
2016-09-16 00:37:19 -0400

    Getting project ready for Heroku

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import weather
+import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -15,4 +16,5 @@ def about():
 	return render_template('about.html')
 
 if __name__ == "__main__":
-    app.run()
+	port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
